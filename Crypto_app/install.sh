d5e1d34d3253dd05285b6b67e80f7f9b52825b57
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2021-06-10 22:10:41 +0100

    Add files via upload

---
+++ Crypto_app/install.sh
@@ -0,0 +1,11 @@
+#!
+#coded by cybereagle2001
+#last time edited Thu 10 Jun 2021 10:09:27 PM CET
+
+clear
+echo "************************************"
+echo "   THIS ACTION MAY TAKE SOME TIME"
+echo "************************************"
+sudo apt-get update 
+sudo apt-get install espeak
+python2 crypto.py
